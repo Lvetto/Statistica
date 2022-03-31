b41ef870ea1c6170fdb96d0bd7125e9dbd032fff
lvetto <luca@DESKTOP-T5MO6J3.localdomain>
2022-03-31 16:22:19 +0200

    Riscritto programma per massimi

--- massimi.py
+++ massimi.py
@@ -1,25 +1,35 @@
 from math import log
 
-infiles = ["g.out", "m.out", "p.out"]
+"""
+    Esportare i file da excel in formato csv, con struttura:
+
+    x1;y1
+    x2;y2
+    ...
+
+"""
+infiles = ["misura1.csv"]    #   Nome dei file
 
 for filename in infiles:
     with open(filename, "r") as file:
         lines = file.readlines()
 
-    lines = [i.split(" ") for i in lines]
+    lines = [i.split(";") for i in lines]
+    lines = [[i.replace(",",".") for i in j if i != '\n'] for j in lines]
+
+    try:
+        float(lines[0][0])
+    except:
+        lines = lines[1:]
     lines = [[float(j) for j in i] for i in lines]
-    #print([i for i in lines])
 
     pos = [i[1] for i in lines]
     times = [i[0] for i in lines]
     pos2 = [sum(pos[n-5:n+5])/10 for n,_ in enumerate(pos[5:-5])][5:]
     maxinds = [n+6 for n, _ in enumerate(pos2[1:-1]) if pos2[n]<pos2[n+1] and pos2[n+1]>pos2[n+2]]
 
-    p0 = sum(pos)/len(pos)
-
-    print("0.1")
-    [print(f"{times[i]} {log(pos[i]-p0)}".replace(".", ".")) for i in maxinds]
     #dts = [times[maxinds[n+1]]-times[maxinds[n]] for n, _ in enumerate(maxinds[1:-1])]
     
-    #print(sum(dts)/len(dts))
+    print("Posizione;Tempo")
+    [print(f"{pos[i]};{times[i]}".replace(".",",")) for i in maxinds]
     print("-"*100)
